5e357540b30e0414ff54522c27948722ac54f0fe
mirat432 <samiragazali10@gmail.com>
2020-09-10 19:41:44 +0100

    Updates

---
+++ precache-manifest.ac1139c88ca1e212264a730efc2e86e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1dafe83df98921841e9df53445926c",
+    "url": "/Assignment6/index.html"
+  },
+  {
+    "revision": "b393d1d55b2a2a6211cc",
+    "url": "/Assignment6/static/css/main.0a0650df.chunk.css"
+  },
+  {
+    "revision": "8f5c1ca833377cdae0a8",
+    "url": "/Assignment6/static/js/2.b71f6cda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assignment6/static/js/2.b71f6cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b393d1d55b2a2a6211cc",
+    "url": "/Assignment6/static/js/main.06402cef.chunk.js"
+  },
+  {
+    "revision": "36360a3d738435c4801f",
+    "url": "/Assignment6/static/js/runtime-main.441cac46.js"
+  }
+]);
