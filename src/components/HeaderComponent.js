f7e22ad3d4c064a0ee2161c252bbf7b6872d4325
mirat432 <samiragazali10@gmail.com>
2020-09-10 19:43:56 +0100

    my

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+//import logo from '../image/logo.PNG';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div className='header'>
+    <p className='navbar'>GHANA COVID-19 UPDATE INFOMATION</p>
+    </div>
+     );
+    }
+}
+
+export default HeaderComponent;
